de0500c12fb0f821ca83bd589b60f38c0f200dfa
Shooter-06 <john190curry@gmail.com>
2026-04-13 14:26:59 -0400

    Iteration 2 PoC updated

--- Iteration 2/project/model/Collaborator.java
+++ Iteration 2/project/model/Collaborator.java
@@ -5,21 +5,20 @@ import java.util.List;
 import model.enums.CollaboratorCategory;
 
 public class Collaborator {
-    private String name;
-    private CollaboratorCategory category;
-    private List<Task>  assignedTask;
-    private int taskLimit;
+    private final String name;
+    private final CollaboratorCategory category;
+    private final List<Task>  assignedTask;
+    public final int taskLimit;
 
     public Collaborator(String name, CollaboratorCategory category) {
         this.name = name;
         this.category = category;
         this.assignedTask = new ArrayList<>();
-        if( category == CollaboratorCategory.JUNIOR) {
-            this.taskLimit = 10;
-        } else if (category == CollaboratorCategory.INTERMEDIATE) {
-            this.taskLimit = 5;
-        } else {
-            this.taskLimit = 2;
+        switch (category) {
+            case JUNIOR -> this.taskLimit = 10;
+            case INTERMEDIATE -> this.taskLimit = 5;
+            case SENIOR -> this.taskLimit = 2;
+            default -> throw new IllegalArgumentException("Unknown category: " + category);
         }
     }
 
@@ -33,7 +32,7 @@ public class Collaborator {
 
     public CollaboratorCategory getCategory() {
         return category;
-    }   
+    }
 
     public List<Task> getAssignedTasks() {
         return assignedTask;
--- Iteration 2/project/model/RecurringPattern.java
+++ Iteration 2/project/model/RecurringPattern.java
@@ -3,13 +3,33 @@ package model;
 import java.util.Date;
 
 public class RecurringPattern {
-    private String frequency;
-    private int interval;
-    private Date startDate;
-    private Date endDate;
+    private final String frequency;
+    private final int interval;
+    private final Date startDate;
+    private final Date endDate;
 
-    public RecurringPattern(String frequency, int interval) {
+    // frequency can be "DAILY", "WEEKLY", "MONTHLY", "YEARLY"
+    public RecurringPattern(String frequency, Date startDate, Date endDate, int interval) {
         this.frequency = frequency;
         this.interval = interval;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
 }
